162d6b76f9b3a7ccf259241141de148610caca5f
jmsr <jmarcosuarez@gmail.com>
2016-11-08 12:35:42 +0000

    Add server API calls

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,8 +2,9 @@ var Repository = require('../../repository/repository');
 var repo = new Repository();
 var User = repo.models.User;
 
+// GET
 function getAllUsers(req, res) {
-  User.findAll({})
+  User.findAll()
     .then(function(users) {
       res.status(200).json(users);
     })
@@ -14,6 +15,36 @@ function getAllUsers(req, res) {
     })
 }
 
+// POST
+function newUser(req, res) {
+  User.create(
+    {
+      gender: 'male/female',
+      title: 'mr/ms',
+      first: req.body.data.first,
+      last: req.body.data.last,
+      email: req.body.data.email,
+      username: req.body.data.username,
+      phone: req.body.data.phone,
+      cell: req.body.data.cell,
+      picture: req.body.data.picture,
+      registered: '1199729925000',
+      dob: '122427907000'
+    })
+    .then(function() {
+      res.status(200).json({
+        success: 1,
+        description: "User added to the list!"
+      });
+    })
+    .catch(function(err) {
+      res.status(500).json({
+        message: err.message,
+      });
+    })
+}
+
+// GET {userId}
 function getUser(req, res) {
   User.findOne({
     where: {
@@ -30,18 +61,52 @@ function getUser(req, res) {
     })
 }
 
-/**
- * Update a specific user on database
- *
- */
+// PUT {userId}
 function setUser(req, res) {
-  User.update(data, {
-    where : {
-      id : req.swagger.params.id.value,
+  User.update(
+    {
+      gender: 'male/female',
+      title: 'mr/ms',
+      first: req.body.data.first,
+      last: req.body.data.last,
+      email: req.body.data.email,
+      username: req.body.data.username,
+      phone: req.body.data.phone,
+      cell: req.body.data.cell,
+      picture: req.body.data.picture,
+      registered: '1199729925000',
+      dob: '122427907000'
+    },
+    {
+      where: {
+        id: req.swagger.params.id.value,
     }
   })
-    .then(function(users) {
-      res.status(200).json(users);
+    .then(function() {
+      res.status(200).json({
+        success: 1,
+        description: "User edited successfully!"
+      });
+    })
+    .catch(function(err) {
+      res.status(500).json({
+        message: err.message,
+      });
+    })
+}
+
+// DELETE {userId}
+function delUser(req, res) {
+  User.destroy({
+    where: {
+      id: req.swagger.params.id.value,
+    }
+  })
+    .then(function() {
+      res.status(200).json({
+        success: 1,
+        description: "User deleted successfully!"
+      });
     })
     .catch(function(err) {
       res.status(500).json({
@@ -53,6 +118,8 @@ function setUser(req, res) {
 
 module.exports = {
   getAllUsers: getAllUsers,
+  newUser: newUser,
   getUser: getUser,
   setUser: setUser,
+  delUser: delUser
 };
