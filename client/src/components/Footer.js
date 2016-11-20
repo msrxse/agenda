ef3b68f2d2094c99462ffa33a0b5aa89eb090a56
jmsr <jmarcosuarez@gmail.com>
2016-11-20 14:23:06 +0000

    Change agenda repository github address

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () =>
   <footer className="info">
     <p>Double-click properties on display to edit</p>
     <p>Written by <a href="https://twitter.com/jmarcosuarez">@jmarcosuarez</a></p>
-    <p>Code in <a href="https://github.com/jmarcosuarez">Contacts</a></p>
+    <p>Code in <a href="https://github.com/jmarcosuarez/agenda">Contacts</a></p>
   </footer>;
 
 export default Footer;
